d7f6fa07199e148c990477d0b48193c79111d9a1
Shahab <safaeishahab@yahoo.com>
2016-05-14 16:21:44 +0430

    Update write.lua

--- plugins/write.lua
+++ plugins/write.lua
@@ -1,8 +1,8 @@
 ﻿-- http://www.messletters.com/fa/
 
 local function run(msg, matches)
-	if not is_admin(msg) then
-		return "این قابلیت فقط مربوط به ادمینهاست"
+	if not is_mod(msg.from.peer_id) then
+		return "این قابلیت فقط مربوط به مدیر هاست"
 	end
 	if #matches < 2 then
 		return "بعد از این دستور، با قید یک فاصله کلمه یا جمله ی مورد نظر را جهت زیبا نویسی وارد کنید"
