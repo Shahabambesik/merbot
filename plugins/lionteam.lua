47373a832e9d1206b60404495bb7fb10da574308
Shahab <safaeishahab@yahoo.com>
2016-05-14 15:03:23 +0430

    Update lionteam.lua

--- plugins/lionteam.lua
+++ plugins/lionteam.lua
@@ -3,7 +3,7 @@ do
 function run(msg, matches)
 local reply_id = msg['id']
 
-local text = 'LionBot V 1.0.0\n\nAn Advance Administration BOT Based On Merbot\n\nAdmins:\n- @Shahabambesik - 119989724\n- @Joy_Face - 128556958\n- @Pho3nix - 70694289\nPowered By LionTeam.ir'
+local text = 'LionBot V 1.0.0\n\nAn Advance Administration BOT Based On Merbot\n\nAdmins:\n- @Shahabambesik - 119989724\n- @Joy_Face - 128556958\n- @Pho3nix - 70694289\n\nOur Channel : https://telegram.me/lionteam\nPowered By LionTeam.ir'
 
 reply_msg(reply_id,  text, ok_cb, false)
 end
