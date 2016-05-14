a7bffa1bcbc0d99c8c5c3d4e600f48597f6b0faf
Shahab <safaeishahab@yahoo.com>
2016-05-14 12:15:49 +0430

    Update joke.lua

--- plugins/joke.lua
+++ plugins/joke.lua
@@ -1,16 +1,19 @@
-do
+local database = 'http://vip.opload.ir/vipdl/94/11/amirhmz/'
+local function run(msg)
+ local res = http.request(database.."joke.db")
+ local joke = res:split(",")
+ local text = joke[math.random(#joke)] 
+ local text = text..'\n\n[Lion Team](http://telegram.me/LionTeam'
+    send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
 
-function run
-if matches[1]:lower() == 'joke' then
-local res = http.request('http://82.102.11.188/joke.db')
---local res = load_from_file('/var/www/html/joke.db', *)
-local joke = res:split("`")
-return joke[math.random(#joke)]..'\n\n@GPMod'
 end
 
 return {
-
-patterns = {     "^joke$"
-}, 
-   run = run  
+ usage = {'<code>!joke</code>',
+         'Funny sentences will be sent randomly'
+         },
+ patterns = {
+  "^[#!/]joke$",
+  },
+ run = run
 }
