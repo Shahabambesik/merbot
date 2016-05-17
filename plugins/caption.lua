abffd333874cb0607123dab2b8c84b8604b9fb0f
Shahab <safaeishahab@yahoo.com>
2016-05-17 22:29:38 +0430

    Update caption.lua

--- plugins/caption.lua
+++ plugins/caption.lua
@@ -1,9 +1,9 @@
 do
-local function action_by_reply22(extra, success, result)
+local function action_by_reply2(extra, success, result)
 if result.media then
 
 
-return  send_msg(extra.receiver, 'caption :\n\n_______________________\n'.. result.media.caption, ok_cb, true)
+return  send_msg(extra.receiver, 'caption :\n\n_______________________\n' result.media.caption, ok_cb, true)
 end
 if result.service then
 return  send_msg(extra.receiver, result.service.type, ok_cb, true)
@@ -15,7 +15,7 @@ if msg.to.type == 'chat' or msg.to.type == 'channel' then
 local receiver = get_receiver(msg) 
    if msg.reply_id then
 
-  msgr = get_message(msg.reply_id, action_by_reply22, {receiver=receiver})
+  msgr = get_message(msg.reply_id, action_by_reply2, {receiver=receiver})
 return 
 
 end
