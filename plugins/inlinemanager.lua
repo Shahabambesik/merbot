8a8efff13e38f6f3055137c6b898c1300d17c526
Shahab <safaeishahab@yahoo.com>
2016-05-29 22:51:19 +0430

    Update inlinemanager.lua

--- plugins/inlinemanager.lua
+++ plugins/inlinemanager.lua
@@ -1,24 +1,23 @@
 local api_key = _config.bot_api.key
 local function run(msg,matches)
     if is_sudo(msg.from.peer_id) then
-    local text = matches[1]
-    local channel_id = '@Shahab_Ch'
-    local link_text = matches[2]
-    local link = matches[3]
+    local text = URL.escape(matches[2])
+    local channel_id = matches[1]  
+  local link_text = URL.escape(matches[3])
+    local link = URL.escape(matches[4])
     local keyboard = '{"inline_keyboard":[[{"text":"'..link_text..'","url":"'..link..'"}]]}'
     local url = 'https://api.telegram.org/bot'..api_key..'/sendMessage?chat_id='..channel_id..'&parse_mode=Markdown&text='..text..'&disable_web_page_preview=true&reply_markup='..keyboard
     local dat, res = https.request(url)
       if res == 400 then
          reply_msg(msg.id, 'Error !', ok_cb, true)    
-         else
+        else
          reply_msg(msg.id, 'Sent !', ok_cb, true)    
       end
       end
   end
   return {
-usage = {'<b>For Sudo</b>'},
       patterns = {
-          "^[!/#]channel (.*) /(.*)/(.*)$"
+          "^[!/#]channel (.*)+(.*)+(.*)+(.*)$"
           },
       run = run
   }
