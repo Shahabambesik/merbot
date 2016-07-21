0847744912a7ce269d331f71f73674c7a1414b64
Shahab <safaeishahab@yahoo.com>
2016-07-21 11:01:30 +0430

    Update test.lua

--- plugins/test.lua
+++ plugins/test.lua
@@ -2,27 +2,27 @@ local function do_keyboard_buygroup()
     local keyboard = {}
     keyboard.inline_keyboard = {
     {
-                {text = 'test', url = 'google.com'},
-                {text = 'test', url = 'google.com'},
+                {text = '🆘Support', url = 'https://telegram.me/joinchat/DJM4TT61WZuuJ5bvjqb4TQ'},
+                {text = '📡Channel', url = 'https://telegram.me/HPRNews'},
       },
     {
-                {text = 'test', url = 'google.com'},
-                {text = 'test', url = 'google.com'},
+                {text = '💵Order Group', url = 'https://telegram.me/joinchat/DJM4TT6uQn7DCH1jZn-rCQ},
+                {text = '🤖Support Bot', url = 'https://http://telegram.me/HprSupportBot'},
       },
    {
-                {text = 'test', url = 'google.com'},
-                {text = 'test', url = 'google.com'},
+                {text = 'Mehran', url = 'https://telegram.me/Mehran_HPR'},
+                {text = 'Shahab', url = 'https://telegram.me/Shahab_Safaei'},
       },
     }
     return keyboard
 end
 local function run(msg,matches)
 local keyboard = do_keyboard_buygroup()
- send_api_keyboard(msg, get_receiver_api(msg), 'test', keyboard)
+ send_api_keyboard(msg, get_receiver_api(msg), 'Hprbot\nAn Advance Administration Bot based on mrrbot', keyboard)
 end
 return {
 patterns = {
-"test"
+"^[Hh][Pp][Rr][Bb][Oo][Tt]$"
 },
 run = run,
 }
